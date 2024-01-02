2f219986237a6eed2a45f63b40589eb827b1314e
Paulis Ratnieks <paulis.ratnieks@di.lv>
2024-01-02 23:05:14 +0200

    6 add wall class, update player, movement, main

--- player.py
+++ player.py
@@ -2,16 +2,18 @@ from shot import Shot
 from movement import Movement
 import pygame as pg
 
+
 class Player(pg.sprite.Sprite, Movement):
     image = pg.Surface((32, 32))
     image.fill((0, 125, 200))
     speed = 4
     shot_cd = 20
     curr_shot_cd = shot_cd
-    
-    def __init__(self, pos, player_shots, *groups):
-        self.rect = self.image.get_rect(center=pos)
-        self.player_shots = player_shots
+
+    def __init__(self, pos, shots, walls, *groups):
+        self.rect = self.image.get_rect(topleft=pos)
+        self.shots = shots
+        self.walls = walls
 
         pg.sprite.Sprite.__init__(self, *groups)
         Movement.__init__(self, self.rect.center, self.speed)
@@ -29,22 +31,38 @@ class Player(pg.sprite.Sprite, Movement):
             self.shoot()
 
         # collision detection        
-        if pg.sprite.spritecollideany(self, self.player_shots):
-            shot = pg.sprite.spritecollideany(self, self.player_shots)
+        if pg.sprite.spritecollideany(self, self.shots):
+            shot = pg.sprite.spritecollideany(self, self.shots)
 
             if shot.shot_by is not self:
                 shot.kill()
                 self.kill()
-                
+
+        if pg.sprite.spritecollideany(self, self.walls):
+            walls = pg.sprite.spritecollide(self, self.walls, False)
+            new_coords = pg.math.Vector2((self.rect.centerx, self.rect.centery))
+
+            for wall in walls:
+                if abs(wall.rect.left - self.rect.right) <= self.speed:
+                    new_coords.x = wall.rect.left - self.rect.size[0] / 2
+                elif abs(wall.rect.right - self.rect.left) <= self.speed:
+                    new_coords.x = wall.rect.right + self.rect.size[0] / 2
+
+                if abs(wall.rect.top - self.rect.bottom) <= self.speed:
+                    new_coords.y = wall.rect.top - self.rect.size[1] / 2
+                elif abs(wall.rect.bottom - self.rect.top) <= self.speed:
+                    new_coords.y = wall.rect.bottom + self.rect.size[1] / 2
+
+                self.rect.center = self.reset_move(new_coords)
 
     def shoot(self):
         if self.curr_shot_cd > 0:
             return
-        
+
         self.curr_shot_cd = self.shot_cd
 
         mouse_x, mouse_y = pg.mouse.get_pos()
         direction = pg.math.Vector2(mouse_x - self.rect.centerx, mouse_y - self.rect.centery)
         if direction.magnitude() > 0:
             direction = direction.normalize() * 3
-            Shot(self, self.rect.center, direction, 5, self.player_shots, *self.groups())
+            Shot(self, self.rect.center, direction, 5, self.shots, *self.groups())
---
+++ wall.py
@@ -0,0 +1,9 @@
+import pygame as pg
+
+class Wall(pg.sprite.Sprite):
+    def __init__(self, pos, size, *groups):
+        self.image = pg.Surface(size)
+        self.image.fill((0, 0, 0))
+        self.rect = self.image.get_rect(topleft=pos)
+        
+        super().__init__(*groups)
