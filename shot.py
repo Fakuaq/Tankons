5fe95a7538d2c8cd270d46dfa9a4e0f5b7d069ac
Paulis Ratnieks <paulis.ratnieks@di.lv>
2024-01-03 23:05:46 +0200

    7 update shot

--- shot.py
+++ shot.py
@@ -1,12 +1,11 @@
 import pygame as pg
-from movement import Movement
 
-class Shot(pg.sprite.Sprite, Movement):
+class Shot(pg.sprite.Sprite):
     life_cd = 60 * 3
     current_life_cd = life_cd
     image = pg.Surface((10, 10))
     image.fill('black')
-    
+
     def __init__(self, player, position, direction, speed, *groups):
         self.shot_by = player
         self.rect = self.image.get_rect(center=position)
@@ -14,12 +13,13 @@ class Shot(pg.sprite.Sprite, Movement):
         self.direction = direction
 
         pg.sprite.Sprite.__init__(self, *groups)
-        Movement.__init__(self, position, self.speed)
 
     def update(self):
-        self.rect.center = self.move(self.direction)
+        # movement
+        direction_normalized = self.direction.normalize() * self.speed
+        self.rect.center += direction_normalized
 
+        # life cd
         self.current_life_cd -= 1
-
         if self.current_life_cd <= 0:
             self.kill()
