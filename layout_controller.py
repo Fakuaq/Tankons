f3d74148c3e1aa6f3763277173d09b23e0038132
Paulis Ratnieks <paulis.ratnieks@di.lv>
2024-01-04 17:29:56 +0200

    8 reformat layout controller

--- layout_controller.py
+++ layout_controller.py
@@ -3,6 +3,7 @@ from random import randint
 from wall import Wall
 import pygame as pg
 
+
 class LayoutController:
     active_layout = False
     walls = []
@@ -10,26 +11,26 @@ class LayoutController:
     
     def __init__(self, *groups):
         self.groups = groups
-        
+
     def generate_layout(self):
         layout_index = randint(0, len(layouts) - 1)
         layout = layouts[layout_index]
-        
+
         for i in range(len(layout['wall_positions'])):
             self.walls.append(Wall(layout['wall_positions'][i], layout['wall_sizes'][i], *self.groups))
-        
+
         for i in range(len(layout['spawns'])):
             self.spawns.append(pg.Rect(layout['spawns'][i]))
-            
+
     def spawn_coordinates(self, player_count):
         if player_count > len(self.spawns):
             raise ValueError('Player count can\'t be bigger than the spawn position count')
-        
+
         if not self.walls:
             raise RuntimeError('Can\'t call method with uninitialized layout')
 
         coords = []
-        for i in range(player_count):
+        for _ in range(player_count):
             spawn_index = randint(0, len(self.spawns) - 1)
             rect = self.spawns[spawn_index]
             self.spawns.pop(spawn_index)
@@ -47,5 +48,3 @@ class LayoutController:
         self.spawns = []
         for wall in self.walls:
             wall.kill()
-            
-            
