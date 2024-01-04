4a549a5c726f7f7b84ff11d0a62e49d59db2d5ea
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-01-04 12:09:56 +0200

    Move image and velocity attribute out of constructor

--- particle.py
+++ particle.py
@@ -2,12 +2,13 @@ import pygame as pg
 import random
 
 class Particle(pg.sprite.Sprite):
+    image = pg.Surface((5, 5))
+    lifetime = 20
+        
     def __init__(self, x, y, color, speed, *groups):
-        self.image = pg.Surface((5, 5))
         self.image.fill(color)
         self.rect = self.image.get_rect(center=(x, y))
         self.velocity = pg.Vector2(random.uniform(-1, 1), random.uniform(-1, 1)).normalize() * speed
-        self.lifetime = 20
         
         pg.sprite.Sprite.__init__(self, *groups)
         
