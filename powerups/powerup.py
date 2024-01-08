a299bf51cefc214f2689c912e9755b985f8ce96e
Reinis Rudens <reinis.rudens.dev@gmail.com>
2024-01-08 21:40:00 +0200

    Add powerup dosctring

--- powerups/powerup.py
+++ powerups/powerup.py
@@ -5,6 +5,26 @@ from typing import Type
 from sound_controller import SoundController
 
 class Powerup(pg.sprite.Sprite):
+    """
+    A class representing a powerup in the game.
+
+    Attributes:
+        - base_path (str): The base path for loading powerup sprites.
+        - powerup (StatsPowerup|ShotPowerup): The type of powerup to be applied.
+        - sprite_path (str): The path to the powerup sprite.
+        - coords (tuple): The coordinates (x, y) of the powerup.
+        - players (pygame.sprite.Group): The group of players in the game.
+        - powerup_sprites (pygame.sprite.Group): The group of powerup sprites.
+        - walls (pygame.sprite.Group): The group of wall sprites.
+        - *groups: Additional sprite groups to which the powerup should belong.
+
+    Methods:
+        - __init__(self, powerup: Type[StatsPowerup|ShotPowerup], sprite_path, coords, players, powerup_sprites, walls, *groups):
+            Initializes the Powerup object with the specified parameters.
+
+        - update(self):
+            Updates the state of the powerup in the game.
+    """
     base_path = 'assets/powerups/'
     
     def __init__(self, powerup: Type[StatsPowerup|ShotPowerup], sprite_path, coords, players, powerup_sprites, walls, *groups):
@@ -19,6 +39,7 @@ class Powerup(pg.sprite.Sprite):
         
     def update(self):
         for player in self.players:
+            # check player and powerup collision
             collided_powerup = pg.sprite.spritecollideany(player, self.powerup_sprites)
             if collided_powerup:
                 powerup = collided_powerup.powerup(player) # instantiate powerup
