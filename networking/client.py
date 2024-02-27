208556d934a5e420dc3188f38b778ef53b907b5f
Paulis Ratnieks <paulis.ratnieks@di.lv>
2024-02-27 20:23:43 +0200

    change to use udp protocol instead of tcp

--- networking/client.py
+++ networking/client.py
@@ -5,8 +5,7 @@ import socket
 import pickle
 from threading import Thread
 
-HOST = '127.0.0.1'
-PORT = 5555
+ADDRESS = ('127.0.0.1', 5555)
 
 
 class Client(Observer):
@@ -15,8 +14,7 @@ class Client(Observer):
     def __init__(self, game_controller):
         GameEventObservable().attach(self)
         self._gc = game_controller
-        self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self._s.connect((HOST, PORT))
+        self._s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
         server_thread = Thread(target=self.listen)
         server_thread.daemon = True
@@ -24,14 +22,14 @@ class Client(Observer):
 
     def listen(self):
         while True:
-            data = self._s.recv(1024)
+            data, _ = self._s.recvfrom(1024)
             data = pickle.loads(data)
             (message_type, value) = data
 
             self.handle_response(message_type, value)
 
     def transmit(self, message_type: GameEvent, value=None):
-        self._s.sendall(pickle.dumps((message_type.value, value)))
+        self._s.sendto(pickle.dumps((message_type.value, value)), ADDRESS)
 
     def handle_response(self, message_type: str, value):
         match message_type:
@@ -61,6 +59,3 @@ class Client(Observer):
         match event_type.value:
             case GameEvent.SHOT.value:
                 self.transmit(GameEvent.SHOT)
-
-    def __del__(self):
-        self._s.close()
--- networking/server.py
+++ networking/server.py
@@ -2,19 +2,15 @@ from threading import Thread
 from enums.game_event import GameEvent
 import pickle
 import socket
-from _thread import *
 from observers.observer import Observer
 from observers.game_event_observable import GameEventObservable
 
-HOST = '127.0.0.1'
-PORT = 5555
-MAX_CLIENT_COUNT = 5
+ADDRESS = ('127.0.0.1', 5555)
 
 
 class Server(Observer):
     _gc = None
     _client_addresses = {}
-    _client_connections = {}
     _client_counter = 0
     _current_layout = None
     _player_coords = {}
@@ -22,11 +18,10 @@ class Server(Observer):
     def __init__(self, game_controller):
         GameEventObservable().attach(self)
         self._gc = game_controller
-        self._s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self._s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-        self._s.bind((HOST, PORT))
-        self._s.listen(MAX_CLIENT_COUNT)
-        print(f'Listening on port: {PORT}, max clients: {MAX_CLIENT_COUNT}')
+        self._s.bind(ADDRESS)
+        print(f'Listening on port: {ADDRESS[1]}')
 
         server_thread = Thread(target=self.run_server)
         server_thread.daemon = True
@@ -34,17 +29,7 @@ class Server(Observer):
 
     def run_server(self):
         while True:
-            conn, addr = self._s.accept()
-            self._client_connections[addr] = conn
-            print(f'Received connection from {addr}')
-            start_new_thread(self.threaded, (conn, addr))
-
-    def threaded(self, conn, addr):
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                print(f'Connection terminated from {addr}')
-                break
+            data, addr = self._s.recvfrom(1024)
 
             try:
                 data = pickle.loads(data)
@@ -55,14 +40,12 @@ class Server(Observer):
             message_type: GameEvent
             (message_type, value) = data
 
-            response = self.handle_response(message_type, value, addr, conn)
+            response = self.handle_response(message_type, value, addr)
 
             if response:
-                conn.send(pickle.dumps((response[0].value, response[1])))
+                self._s.sendto(pickle.dumps((response[0].value, response[1])), addr)
 
-        conn.close()
-
-    def handle_response(self, message_type: GameEvent, value, addr, conn):
+    def handle_response(self, message_type: GameEvent, value, addr):
         player_index = None
         for k, v in self._client_addresses.items():
             if v == addr:
@@ -76,7 +59,7 @@ class Server(Observer):
                 self._player_coords[self._client_counter] = (0, 0, 0)
                 print(f'Joined player nr.{self._client_counter}')
 
-                conn.send(pickle.dumps((GameEvent.JOIN.value, self._client_counter)))
+                self._s.sendto(pickle.dumps((GameEvent.JOIN.value, self._client_counter)), addr)
 
                 if self._client_counter >= self._gc.player_count:
                     self._current_layout = self._gc.pick_layout()
@@ -101,13 +84,13 @@ class Server(Observer):
                 self.broadcast_except(addr, GameEvent.SHOT, player_index)
 
     def broadcast(self, message_type: GameEvent, value=None):
-        for conn in self._client_connections.values():
-            conn.send(pickle.dumps((message_type.value, value)))
+        for addr in self._client_addresses.values():
+            self._s.sendto(pickle.dumps((message_type.value, value)), addr)
 
     def broadcast_except(self, address, message_type: GameEvent, value=None):
-        for key, conn in self._client_connections.items():
-            if key != address:
-                conn.send(pickle.dumps((message_type.value, value)))
+        for addr in self._client_addresses.values():
+            if addr != address:
+                self._s.sendto(pickle.dumps((message_type.value, value)), addr)
 
     def update(self, observable: GameEventObservable):
         (event_type, event_value) = observable.game_event()
